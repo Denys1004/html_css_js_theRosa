d65d57ed25d1b68b6c29c996c460a3d49c31cde9
denys krychevskyi <krychevskyidenys@gmail.com>
2020-08-06 09:25:01 -0700

    DONE

--- the-rosa/main.js
+++ the-rosa/main.js
@@ -7,3 +7,34 @@ let body = selectElement('body');
 menuToggler.addEventListener('click', function(){
     body.classList.toggle('open');
 })
+
+// Animation on scroll
+
+window.sr = ScrollReveal(); // we can access scroll reveal by referring as sr
+sr.reveal('.animate-left', {
+    origin: 'left',         // origin means where this animation coming from 
+    duration: 1000,         // duration in milliseconds
+    distance: '25rem',      // means how far this traveling
+    delay: 300              // means how long should it take before animation is triggered, in milliseconds
+})
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+})
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+})
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+})
+
